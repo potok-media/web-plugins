dc8e26e240b8270407b2f6f03a3009d096327726
egorrr_miller <44502536+egorrrmiller@users.noreply.github.com>
2026-07-09 17:29:27 +0300

    torrent

--- torrents-plugin/utils/searchEngineSettings.js
+++ torrents-plugin/utils/searchEngineSettings.js
@@ -1,5 +1,5 @@
 import { PotokSDK } from 'potok-sdk';
-import { resolveSearchEngineApiKey, resolveSearchEngineUrl, searchEngineHeaders } from './config.js';
+import { resolveSearchEngineUrl, searchEngineHeaders } from './config.js';
 
 function parseCategories(value) {
   if (value === undefined || value === "") return [];
@@ -9,8 +9,22 @@ function parseCategories(value) {
     .filter((num) => !Number.isNaN(num));
 }
 
-export function fileConfigToSettings(config) {
+function authFields(authorization) {
   return {
+    login: authorization?.login ?? "",
+    password: authorization?.password ?? "",
+  };
+}
+
+export function fileConfigToSettings(config) {
+  const rutrackerAuth = authFields(config.rutracker?.authorization);
+  const animelayerAuth = authFields(config.animelayer?.authorization);
+  const kinozalAuth = authFields(config.kinozal?.authorization);
+  const ffprobeAuth = authFields(config.ffprobe?.authorization);
+  const proxyItem = config.proxy?.list?.[0] ?? {};
+
+  return {
+    searchEngineApiKey: config.apiKey ?? "",
     mergeDuplicates: config.mergeDuplicates,
     mergeNumDuplicates: config.mergeNumDuplicates,
     cacheEnable: config.cache.enable,
@@ -25,25 +39,38 @@ export function fileConfigToSettings(config) {
     ffprobeTsUri: config.ffprobe.tsUri ?? "",
     ffprobeBatchSize: config.ffprobe.batchSize,
     ffprobeAttempts: config.ffprobe.attempts,
+    ffprobeLogin: ffprobeAuth.login,
+    ffprobePassword: ffprobeAuth.password,
     proxyBypassOnLocal: config.proxy.bypassOnLocal,
-    proxyUrl: config.proxy.list[0]?.url ?? "",
+    proxyUrl: proxyItem.url ?? "",
+    proxyUsername: proxyItem.username ?? "",
+    proxyPassword: proxyItem.password ?? "",
     rutrackerEnableSearch: config.rutracker.enableSearch,
     rutrackerPopularEnable: config.rutracker.popular.enable,
     rutrackerPopularTimeout: config.rutracker.popular.timeOut,
     rutrackerPopularMaxPages: config.rutracker.popular.maxPages,
     rutrackerPopularCategories: config.rutracker.popular.categories.join(", "),
+    rutrackerLogin: rutrackerAuth.login,
+    rutrackerPassword: rutrackerAuth.password,
     animelayerEnableSearch: config.animelayer.enableSearch,
+    animelayerLogin: animelayerAuth.login,
+    animelayerPassword: animelayerAuth.password,
     nnmclubEnableSearch: config.nnmclub.enableSearch,
     rutorEnableSearch: config.rutor.enableSearch,
     anilibertyEnableSearch: config.aniliberty.enableSearch,
     kinozalEnableSearch: config.kinozal.enableSearch,
+    kinozalLogin: kinozalAuth.login,
+    kinozalPassword: kinozalAuth.password,
     megapeerEnableSearch: config.megapeer.enableSearch,
   };
 }
 
 export function settingsToFileConfig(settings) {
   const proxyUrl = String(settings.proxyUrl ?? "").trim();
+  const apiKey = String(settings.searchEngineApiKey ?? "").trim();
+
   return {
+    apiKey: apiKey || null,
     mergeDuplicates: !!settings.mergeDuplicates,
     mergeNumDuplicates: !!settings.mergeNumDuplicates,
     cache: {
@@ -63,13 +90,27 @@ export function settingsToFileConfig(settings) {
       tsUri: String(settings.ffprobeTsUri ?? ""),
       batchSize: Number(settings.ffprobeBatchSize ?? 20),
       attempts: Number(settings.ffprobeAttempts ?? 3),
+      authorization: {
+        login: String(settings.ffprobeLogin ?? ""),
+        password: String(settings.ffprobePassword ?? ""),
+      },
     },
     proxy: {
       bypassOnLocal: !!settings.proxyBypassOnLocal,
-      list: proxyUrl ? [{ url: proxyUrl }] : [],
+      list: proxyUrl
+        ? [{
+            url: proxyUrl,
+            username: String(settings.proxyUsername ?? ""),
+            password: String(settings.proxyPassword ?? ""),
+          }]
+        : [],
     },
     rutracker: {
       enableSearch: !!settings.rutrackerEnableSearch,
+      authorization: {
+        login: String(settings.rutrackerLogin ?? ""),
+        password: String(settings.rutrackerPassword ?? ""),
+      },
       popular: {
         enable: !!settings.rutrackerPopularEnable,
         timeOut: Number(settings.rutrackerPopularTimeout ?? 600),
@@ -77,11 +118,23 @@ export function settingsToFileConfig(settings) {
         categories: parseCategories(settings.rutrackerPopularCategories),
       },
     },
-    animelayer: { enableSearch: !!settings.animelayerEnableSearch },
+    animelayer: {
+      enableSearch: !!settings.animelayerEnableSearch,
+      authorization: {
+        login: String(settings.animelayerLogin ?? ""),
+        password: String(settings.animelayerPassword ?? ""),
+      },
+    },
     nnmclub: { enableSearch: !!settings.nnmclubEnableSearch },
     rutor: { enableSearch: !!settings.rutorEnableSearch },
     aniliberty: { enableSearch: !!settings.anilibertyEnableSearch },
-    kinozal: { enableSearch: !!settings.kinozalEnableSearch },
+    kinozal: {
+      enableSearch: !!settings.kinozalEnableSearch,
+      authorization: {
+        login: String(settings.kinozalLogin ?? ""),
+        password: String(settings.kinozalPassword ?? ""),
+      },
+    },
     megapeer: { enableSearch: !!settings.megapeerEnableSearch },
   };
 }
